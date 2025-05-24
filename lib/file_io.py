df18971b93f09e790922a67ad45d884d4b21d568
Ian Sang <sangian006@gmail.com>
2025-05-24 12:46:27 +0300

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    """Write content to a .txt file"""
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Append content to an existing .txt file"""
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """Read and return the content of a .txt file"""
+    with open(f"{file_name}.txt", encoding="utf-8") as file:
+        return file.read()
